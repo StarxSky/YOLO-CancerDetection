de4ce8f7816cb3c07be6b627036e3b9f4bb85413
StarxSky <62407841+StarxSky@users.noreply.github.com>
2026-01-03 02:10:49 +0800

    add MPS backends

--- model.py
+++ model.py
@@ -74,8 +74,14 @@ def parse_args():
     return parser.parse_args()
 
 # Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')
+else :
+    device = torch.device('cpu')
+    
 
             
 ###         DATASET AND DATA LOADING FUNCTIONS        ###
